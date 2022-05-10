55ffd9c446f0b9cd33bcccbea5f401df5e912bd1
Евгений <@DESKTOP-SJVT4QK>
2022-05-10 13:56:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Иванов";
+            string surname = "Иван";
+            Console.WriteLine($"Ваше имя {name}, фамилия {surname}");
+            string name1;
+            name1 = name;
+            name = surname;
+            surname = name1;
+            Console.WriteLine($"Ваше имя {name}, фамилия {surname}");
+        }
+    }
+}
